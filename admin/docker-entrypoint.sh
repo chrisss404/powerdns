b54ffe78e7e47e99ed9005c98277b8744d47218c
Christian Hofer <chrisss404@gmail.com>
2019-12-15 13:12:43 +0100

    update admin build, switch from uwsgi to gunicorn

--- admin/docker-entrypoint.sh
+++ admin/docker-entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = "uwsgi" ] && [ ! -f /var/www/pdns-admin/config.py ]; then
-    cp /var/www/pdns-admin/config_template.py /var/www/pdns-admin/config.py
+if [ "$1" = "gunicorn" ] && [ ! -f /var/www/pdns-admin/powerdnsadmin/docker_config.py ]; then
+    cat /var/www/pdns-admin/powerdnsadmin/default_config.py /var/www/pdns-admin/docker_config.py > /var/www/pdns-admin/powerdnsadmin/docker_config.py
 
-    sed -i "s|LOG_FILE = 'logfile.log'|LOG_FILE = ''|g" /var/www/pdns-admin/config.py
-    sed -i "s|SQLA_DB_USER = 'pda'|SQLA_DB_USER = '${ADMIN_DB_USER:-pda}'|g" /var/www/pdns-admin/config.py
-    sed -i "s|SQLA_DB_PASSWORD = 'changeme'|SQLA_DB_PASSWORD = '${ADMIN_DB_PASS:-pda}'|g" /var/www/pdns-admin/config.py
-    sed -i "s|SQLA_DB_HOST = '127.0.0.1'|SQLA_DB_HOST = '${ADMIN_DB_HOST:-admin-db}'|g" /var/www/pdns-admin/config.py
-    sed -i "s|SQLA_DB_PORT = 3306|SQLA_DB_PORT = ${ADMIN_DB_PORT:-5432}|g" /var/www/pdns-admin/config.py
-    sed -i "s|SQLA_DB_NAME = 'pda'|SQLA_DB_NAME = '${ADMIN_DB_NAME:-pda}'|g" /var/www/pdns-admin/config.py
-    sed -i "s|SQLALCHEMY_DATABASE_URI = 'mysql://'+SQLA_DB_USER+':'+SQLA_DB_PASSWORD+'@'+SQLA_DB_HOST+':'+str(SQLA_DB_PORT)+'/'+SQLA_DB_NAME|SQLALCHEMY_DATABASE_URI = 'postgresql://'+SQLA_DB_USER+':'+SQLA_DB_PASSWORD+'@'+SQLA_DB_HOST+':'+str(SQLA_DB_PORT)+'/'+SQLA_DB_NAME|g" /var/www/pdns-admin/config.py
+    sed -i "s|SQLA_DB_USER = 'pda'|SQLA_DB_USER = '${ADMIN_DB_USER:-pda}'|g" /var/www/pdns-admin/powerdnsadmin/docker_config.py
+    sed -i "s|SQLA_DB_PASSWORD = 'changeme'|SQLA_DB_PASSWORD = '${ADMIN_DB_PASS:-pda}'|g" /var/www/pdns-admin/powerdnsadmin/docker_config.py
+    sed -i "s|SQLA_DB_HOST = '127.0.0.1'|SQLA_DB_HOST = '127.0.0.1'\nSQLA_DB_PORT = ${ADMIN_DB_PORT:-5432}|g" /var/www/pdns-admin/powerdnsadmin/docker_config.py
+    sed -i "s|SQLA_DB_HOST = '127.0.0.1'|SQLA_DB_HOST = '${ADMIN_DB_HOST:-admin-db}'|g" /var/www/pdns-admin/powerdnsadmin/docker_config.py
+    sed -i "s|SQLA_DB_NAME = 'pda'|SQLA_DB_NAME = '${ADMIN_DB_NAME:-pda}'|g" /var/www/pdns-admin/powerdnsadmin/docker_config.py
+    sed -i "s|SQLALCHEMY_DATABASE_URI = 'mysql://'+SQLA_DB_USER+':'+SQLA_DB_PASSWORD+'@'+SQLA_DB_HOST+'/'+SQLA_DB_NAME|SQLALCHEMY_DATABASE_URI = 'postgresql://'+SQLA_DB_USER+':'+SQLA_DB_PASSWORD+'@'+SQLA_DB_HOST+':'+str(SQLA_DB_PORT)+'/'+SQLA_DB_NAME|g" /var/www/pdns-admin/powerdnsadmin/docker_config.py
 
     attempts=0
     while ! psql "host=${ADMIN_DB_HOST:-admin-db} dbname=${ADMIN_DB_NAME:-pda} user=${ADMIN_DB_USER:-pda} password=${ADMIN_DB_PASS:-pda} port=${ADMIN_DB_PORT:-5432}" >/dev/null 2>&1; do
--- admin/update_db_settings.py
+++ admin/update_db_settings.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 
-from app.models import Setting, User
+import logging
 import os
 
+from powerdnsadmin import create_app
+from powerdnsadmin.models.setting import Setting
+from powerdnsadmin.models.user import User
+
 if __name__ == '__main__':
-    Setting().set('pdns_api_url', os.environ.get('ADMIN_PDNS_API_URL', 'http://authoritative:8081/'))
-    Setting().set('pdns_api_key', os.environ.get('ADMIN_PDNS_API_KEY', 'pdns'))
-    Setting().set('pdns_version', os.environ.get('ADMIN_PDNS_VERSION', '4.2.1'))
-    Setting().set('signup_enabled', os.environ.get('ADMIN_SIGNUP_ENABLED', 'no').lower() == 'yes')
+    app = create_app()
+    app.logger.setLevel(logging.INFO)
+    with app.app_context():
+        Setting().set('pdns_api_url', os.environ.get('ADMIN_PDNS_API_URL', 'http://authoritative:8081/'))
+        Setting().set('pdns_api_key', os.environ.get('ADMIN_PDNS_API_KEY', 'pdns'))
+        Setting().set('pdns_version', os.environ.get('ADMIN_PDNS_VERSION', '4.2.1'))
+        Setting().set('signup_enabled', os.environ.get('ADMIN_SIGNUP_ENABLED', 'no').lower() == 'yes')
 
-    firstname = os.environ.get('ADMIN_USER_FIRSTNAME', 'Administrator')
-    lastname = os.environ.get('ADMIN_USER_LASTNAME', 'User')
-    password = os.environ.get('ADMIN_USER_PASSWORD', 'admin')
-    email = os.environ.get('ADMIN_USER_EMAIL', 'admin@example.org')
+        firstname = os.environ.get('ADMIN_USER_FIRSTNAME', 'Administrator')
+        lastname = os.environ.get('ADMIN_USER_LASTNAME', 'User')
+        password = os.environ.get('ADMIN_USER_PASSWORD', 'admin')
+        email = os.environ.get('ADMIN_USER_EMAIL', 'admin@example.org')
 
-    user = User(username='admin', plain_text_password=password, email=email, firstname=firstname, lastname=lastname)
-    if not user.create_local_user()['status']:
-        user.update_local_user()
+        user = User(username='admin', plain_text_password=password, email=email, firstname=firstname, lastname=lastname)
+        if not user.create_local_user()['status']:
+            user.update_local_user()
