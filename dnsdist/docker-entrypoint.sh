28ea15925156f9bd8847e68627da4fe6b2d172ad
Christian Hofer <chrisss404@gmail.com>
2022-03-10 17:19:42 +0100

    dnsdist: fix webserver configuration

--- dnsdist/docker-entrypoint.sh
+++ dnsdist/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = "dnsdist" ] && [ ! -f /etc/dnsdist/dnsdist.conf ]; then
     mv /etc/dnsdist/dnsdist.conf-dist /etc/dnsdist/dnsdist.conf
 
-    sed -i "s|    webserver(\"0.0.0.0:8083\", \"pdns\", \"pdns\")|    webserver(\"0.0.0.0:8083\", \"${DNSDIST_WEBSERVER_PASSWORD:-pdns}\", \"${DNSDIST_API_KEY:-pdns}\")|g" /etc/dnsdist/dnsdist.conf
+    sed -i "s|    setWebserverConfig({password=\"pdns\", apiKey=\"pdns\", acl=\"0.0.0.0/0\"})|    setWebserverConfig({password=\"${DNSDIST_WEBSERVER_PASSWORD:-pdns}\", apiKey=\"${DNSDIST_API_KEY:-pdns}\", acl=\"0.0.0.0/0\"})|g" /etc/dnsdist/dnsdist.conf
 fi
 
 exec "$@"
