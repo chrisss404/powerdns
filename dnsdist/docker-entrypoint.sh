811020dcb0884b796b35e63602ba39439084a313
Christian Hofer <chrisss404@gmail.com>
2025-01-09 15:46:24 +0100

    dnsdist: update control socket handling

--- dnsdist/docker-entrypoint.sh
+++ dnsdist/docker-entrypoint.sh
@@ -3,14 +3,16 @@ set -e
 
 if [ "$1" = "dnsdist" ] && [ ! -f /etc/dnsdist/dnsdist.conf ]; then
     mv /etc/dnsdist/dnsdist.conf-dist /etc/dnsdist/dnsdist.conf
-    CONSOLE_KEY=`dd if=/dev/urandom bs=1 count=32 status=none | base64`
 
-    sed -i "s|setKey(\"replaceMe\")|setKey(\"${CONSOLE_KEY}\")|g" /etc/dnsdist/dnsdist.conf
     sed -i "s|    setWebserverConfig({password=\"pdns\", apiKey=\"pdns\", acl=\"0.0.0.0/0\"})|    setWebserverConfig({password=\"${DNSDIST_WEBSERVER_PASSWORD:-pdns}\", apiKey=\"${DNSDIST_API_KEY:-pdns}\", acl=\"0.0.0.0/0\"})|g" /etc/dnsdist/dnsdist.conf
+fi
 
-    echo "-- Expose commandline console" > /etc/dnsdist/control-socket.conf
-    echo "controlSocket('127.0.0.1:5199')" >> /etc/dnsdist/control-sockett.conf
-    echo "setKey(\"${CONSOLE_KEY}\")" >> /etc/dnsdist/control-socket.conf
+if [ "$1" = "dnsdist" ] && [ ! -f /etc/dnsdist/control-socket.conf ]; then
+    mv /etc/dnsdist/control-socket.conf-dist /etc/dnsdist/control-socket.conf
+
+    CONSOLE_KEY=`dd if=/dev/urandom bs=1 count=32 status=none | base64`
+    sed -i "s|setKey(\".*\")|setKey(\"${CONSOLE_KEY}\")|g" /etc/dnsdist/control-socket.conf
+    sed -i "s|setKey(\".*\")|setKey(\"${CONSOLE_KEY}\")|g" /etc/dnsdist/dnsdist.conf
 fi
 
 exec "$@"
